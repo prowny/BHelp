bfdb8596dfa4c347acc1e9b171d7c489a9a54bd1
Peter Rowny <prowny@aol.com>
2021-04-27 15:31:39 -0400

    Initial Commit

---
+++ BHelp/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BHelp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Date of Birth")]
+        [Column(TypeName = "Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+        public Location LocationID { get; set; }
+ 
+    }
+}
---
+++ BHelp/Models/Location.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel .DataAnnotations ;
+namespace BHelp.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Street Number")]
+        public string StreetNumber { get; set; }
+
+        [Required]
+        [DisplayName("Street Name")]
+        public string StreetName { get; set; }
+
+        [Required]
+        [DisplayName("City")]
+        public string City { get; set; }
+
+        [Required]
+        [DisplayName("Zip Code")]
+        public string Zip { get; set; }
+
+    }
+}
