fe163b433eafe2f85c31786b21ab7d472dc1bf86
Peter Rowny <prowny@aol.com>
2021-04-29 07:33:56 -0400

    Add Ientity Account

---
+++ BHelp/DataAccessLayer/BHelpContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using BHelp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Diagnostics;
+
+namespace BHelp.DataAccessLayer
+{
+    public class BHelpContext : IdentityDbContext<ApplicationUser>
+    {
+        public static void EnableMigrations()
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<BHelpContext>());
+        }
+
+        public static void DropAndCreateDatabase()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<BHelpContext>());
+        }
+
+        public BHelpContext() : base("BHelpContext")
+        {
+            Database.Log = msg => Debug.Write(msg);
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Login> Login { get; set; }
+
+        //public DbSet<Staff> StaffMembers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public static BHelpContext Create()
+        {
+            return new BHelpContext();
+        }
+
+    }
+
+}
---
+++ BHelp/Models/ApplicationUser.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+//using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace BHelp.Models
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ApplicationUser : IdentityUser
+    {
+        [JsonProperty, DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty, DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Title")]
+        public string Title { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+        //[InverseProperty("PrimaryTutor")]
+        //public virtual ICollection<Student> PrimaryTutees { get; set; }
+
+        [JsonProperty]
+        public override string Email
+        {
+            get
+            {
+                return base.Email;
+            }
+            set
+            {
+                base.Email = value;
+            }
+        }
+        [JsonProperty]
+        public override string Id
+        {
+            get
+            {
+                return base.Id;
+            }
+            set
+            {
+                base.Id = value;
+            }
+        }
+        [JsonProperty]
+        public override string UserName
+        {
+            get
+            {
+                return base.UserName;
+            }
+            set
+            {
+                base.UserName = value;
+            }
+        }
+        [JsonProperty]
+        public override string PhoneNumber
+        {
+            get
+            {
+                return base.PhoneNumber;
+            }
+            set
+            {
+                base.PhoneNumber = value;
+            }
+        }
+
+        //// For use by TutorSchedule update routines:
+        //[NotMapped]
+        //public List<TutorSchedule> TutorSchedules { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+    }
+}
