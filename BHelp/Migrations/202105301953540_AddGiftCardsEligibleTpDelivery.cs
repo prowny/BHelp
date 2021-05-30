c2b8d5e82d58e8b4fb703f05042ba7a78e362ab7
Peter Rowny <prowny@aol.com>
2021-05-30 16:04:12 -0400

    Update-database

---
+++ BHelp/Migrations/202105301953540_AddGiftCardsEligibleTpDelivery.cs
@@ -0,0 +1,18 @@
+﻿namespace BHelp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddGiftCardsEligibleTpDelivery : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Deliveries", "GiftCardsEligible", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Deliveries", "GiftCardsEligible");
+        }
+    }
+}
