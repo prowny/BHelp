014da5b9c2a48dca06078243e43d3e02709a5476
Peter Rowny <prowny@aol.com>
2021-05-31 21:43:25 -0400

    Modify Edir Delivery

--- BHelp/Global.asax.cs
+++ BHelp/Global.asax.cs
@@ -19,7 +19,7 @@ namespace BHelp
         protected void Session_Start(Object sender, EventArgs e)
         {
             HttpContext.Current.Session.Add("CurrentUserFullName", "");  // (initialize variable)
-            DateTime cdt = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day +1);  // (initialize variable)
+            DateTime cdt =DateTime.Today.AddDays(1); // (initialize variable to tomorrow's date)
             var callLogDate = cdt.ToString("MM/dd/yyyy");
             HttpContext.Current.Session.Add("CallLogDate", callLogDate); 
         }
