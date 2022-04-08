820cc05b74c0e9f7882d7a417527bbdfb0181eb1
Peter Rowny <prowny@aol.com>
2022-04-08 17:37:09 -0400

    Driver Schedule

--- BHelp/Controllers/DriverScheduleController.cs
+++ BHelp/Controllers/DriverScheduleController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using BHelp.ViewModels;
+using DocumentFormat.OpenXml.Drawing;
+using Org.BouncyCastle.Utilities;
 
 namespace BHelp.Controllers
 {
@@ -8,23 +11,49 @@ namespace BHelp.Controllers
         // GET: DriverSchedule
         public ActionResult Index()
         {
-            var view = new DriverScheduleViewModel()
+            var view = new DriverScheduleViewModel();
+            if (Session["DriverScheduleMonthYear"] == null)
             {
-                MonthName = "APRIL",
-                Year = 2022
-            };
+                view.Month = DateTime.Today.Month;
+                view.Year = DateTime.Today.Year;
+                view.MonthName = Strings.ToUpperCase(DateTime.Today.ToString("MMMM"));
+                Session["DriverScheduleMonthYear"] = view.Month.ToString("00") + view.Year;
+            }
+            else
+            {
+                var x = Session["DriverScheduleMonthYear"].ToString();
+                view.Month = Convert.ToInt32(x.Substring(0, 2));
+                view.Year = Convert.ToInt32(x.Substring(2, 4));
+                var fakeDate = new DateTime(view.Year, view.Month, 1);
+                view.MonthName = Strings.ToUpperCase(fakeDate.ToString("MMMM"));
+            }
+        
 
             return View(view);
         }
 
         public ActionResult PreviousMonth(int month, int year)
         {
-            return null;
+            month = month - 1;
+            if (month < 1)
+            {
+                month = 12;
+                year = year - 1;
+            }
+            Session["DriverScheduleMonthYear"] = month.ToString("00") + year;
+            return RedirectToAction( "Index");
         }
 
         public ActionResult NextMonth(int month, int year)
         {
-            return null;
+            month = month + 1;
+            if (month >12)
+            {
+                month = 1;
+                year = year + 1;
+            }
+            Session["DriverScheduleMonthYear"] = month.ToString("00") + year;
+            return RedirectToAction("Index");
         }
     }
 }
