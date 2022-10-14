9f0abb349ea92b70c802359b8f88156fe91000fc
Peter Rowny <prowny@aol.com>
2022-10-13 22:18:35 -0400

    Fix Open Deliveries Screen Sort

--- BHelp/Controllers/DeliveriesController.cs
+++ BHelp/Controllers/DeliveriesController.cs
@@ -617,7 +617,7 @@ namespace BHelp.Controllers
                 var listAllOpenDeliveries = db.Deliveries.Where(d => d.Status == 0)
                     .OrderBy(d => d.DateDelivered).ThenBy(z => z.Zip)
                     // added 10/13/2022:
-                    .ThenBy(s => s.StreetName).ThenBy(n => n.StreetNumber)
+                    .ThenBy(n => n.StreetNumber).ThenBy(s => s.StreetName)
                     .ThenBy(n => n.LastName).ToList();   // get all open deliveries
                 if (view.ReplacementDriverId == "0") view.ReplacementDriverId = null;
                 var newView = new OpenDeliveryViewModel
