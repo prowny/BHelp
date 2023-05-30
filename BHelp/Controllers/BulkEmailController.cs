92db745e57bb6e5469fcffeb0f791d8961744e50
Peter Rowny <prowny@aol.com>
2023-05-30 18:12:17 -0400

    Email system cosmetics

---
+++ BHelp/Controllers/BulkEmailController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BHelp.Controllers
+{
+    public class BulkEmailController : Controller
+    {
+        // GET: BulkEmail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
